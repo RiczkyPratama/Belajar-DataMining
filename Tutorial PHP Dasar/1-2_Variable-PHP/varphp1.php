0a9b487608db7651d23f5b75d2ea8d316dd69ee6
RiczkyPratama <furatamarizuki@gmail.com>
2024-01-05 01:02:01 +0700

    ini commit 1.2

---
+++ Tutorial PHP Dasar/1-2_Variable-PHP/varphp1.php
@@ -0,0 +1,24 @@
+<?php
+
+// 1.1 Cara Penulisan Variable
+$nama;
+
+// 1.2 Mengisi Nilai dari Suatu Variable
+$nama = "Riczky Pratama";
+$umur = 21;
+
+// 1.3 Contoh Implementasi Variable
+$nama1 = "Riczky Pratama";
+$umur1 = 30;
+echo "</br>";
+echo $nama1;
+echo "</br>";
+echo $umur1;
+echo "</br>";
+echo "</br>";
+echo "Perkenalkan Nama Saya " .$nama1. " dan Umur Saya " .$umur1;
+
+// Penjelasan ada di https://github.com/RiczkyPratama/Belajar-DataMining
+// Dukung developer di https://saweria.co/sikelinciperak
+
+?>
